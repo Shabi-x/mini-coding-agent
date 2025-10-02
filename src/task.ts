b099d915f50133fd73617eb1abe3eb017041b26b
Shabix <2936670268@qq.com>
2025-10-03 04:13:23 +0800

    feat: 类型体操

--- src/task.ts
+++ src/task.ts
@@ -5,13 +5,27 @@ import { OpenAI } from "openai";
 import { readFile } from "node:fs/promises";
 import * as path from "path";
 
+const toolName = ["attempt_completion", "read_file"] as const;
+const paramsName = ["result", "filename"] as const;
+export type ToolName = (typeof toolName)[number];
+export type ParamsName = (typeof paramsName)[number];
+type ToolUse = {
+  type: "tool_use";
+  name: ToolName;
+  params: Partial<Record<ParamsName, string>>;
+};
+type TextContent = {
+  type: "text";
+  content: string;
+};
+type AssistantMessageContent = ToolUse | TextContent;
 // 实现与大模型交互相关的逻辑
 export class Task {
   private systemPrompt: string = "";
   constructor(
     private provider: AgentWebviewProvider,
     private apiConfiguration: ApiConfiguration,
-    private message: string,
+    private message: string
   ) {
     const { model, apiKey, BaseUrl } = this.apiConfiguration;
   }
@@ -25,12 +39,16 @@ export class Task {
       },
     ];
     const stream = apiHandler.createMessage(systemPrompt, messages);
+    let assistantMessage: string = "";
     for await (const chunk of stream) {
       console.log(chunk);
+      assistantMessage += chunk;
       const messageStr =
         typeof chunk === "string" ? chunk : JSON.stringify(chunk);
       this.provider.postMessage(messageStr);
     }
+    const assistantContent = this.ParseAssistantMessage(assistantMessage);
+    this.PresentAssistantMessage();
   }
 
   async GetSystemPrompt() {
@@ -46,5 +64,14 @@ export class Task {
       "utf-8"
     );
     return this.systemPrompt;
+  }
+
+  // 解析大模型返回的消息，提取出代码块
+  ParseAssistantMessage(assistantMessage: string): AssistantMessageContent[] {
+    return [];
+  }
+
+  // 展示大模型返回的代码块
+  PresentAssistantMessage() {
   }
 }
